4a076fcf06cf646f385142aba53bfc7ca7dc4714
islem629 <islemhamdi368@gmail.com>
2025-12-01 17:42:49 +0100

    Update auth service config and pom

--- src/main/java/com/iset/AuthServiceApplication.java
+++ src/main/java/com/iset/AuthServiceApplication.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.iset", "com.iset.config"})
 @EntityScan(basePackages = "com.iset.model")
+
 @EnableJpaRepositories(basePackages = "com.iset.repository")
 public class AuthServiceApplication {
 
